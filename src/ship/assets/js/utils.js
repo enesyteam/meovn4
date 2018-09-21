b7e6c410e9b2e6edbd664a7bdd68af0cd418f489
Nguyễn Văn Công <congnvc@gmail.com>
2018-09-21 09:55:34 +0700

    update code

--- src/ship/assets/js/utils.js
+++ src/ship/assets/js/utils.js
@@ -32,49 +32,50 @@
                             reject(w + ' bắt buộc');
                         }
                         if (!IsNullOrEmpty(l)) {
-                            if (l.indexOf("1800") === 0 || l.indexOf("1900") === 0) {
-                                reject('Đây là đầu số 1800, 1900');
-                            }
-                            if (l[0] == "0" && (l = l.substr(1, l.length)), p = q.split(","), d = 0, p != null && p != undefined && p.length > 0) {
-                                for (g = 0; g < p.length; g++) {
-                                    if (l.indexOf(p[g]) === 0) {
-                                        d = p[g];
-                                        break
-                                    }
-                                }
-                                if (g < p.length && !isNaN(l)) {
-                                    var result = d == "868" && l.length != 9 ? "Số điện thoại phải là 10 số" : d != "868" && d.length == 2 && l.length != 9 ? "Số điện thoại phải là 10 số" : d != "868" 
-                                    && d.length == 3 && l.length != 10 ? "Số điện thoại phải là 11 số" : ''
+                            // if (l.indexOf("1800") === 0 || l.indexOf("1900") === 0) {
+                            //     reject('Đây là đầu số 1800, 1900');
+                            // }
+                            // if (l[0] == "0" && (l = l.substr(1, l.length)), p = q.split(","), d = 0, p != null && p != undefined && p.length > 0) {
+                            //     for (g = 0; g < p.length; g++) {
+                            //         if (l.indexOf(p[g]) === 0) {
+                            //             d = p[g];
+                            //             break
+                            //         }
+                            //     }
+                            //     if (g < p.length && !isNaN(l)) {
+                            //         var result = d == "868" && l.length != 9 ? "Số điện thoại phải là 10 số" : d != "868" && d.length == 2 && l.length != 9 ? "Số điện thoại phải là 10 số" : d != "868" 
+                            //         && d.length == 3 && l.length != 10 ? "Số điện thoại phải là 11 số" : ''
 
-                                    if(result !== ''){
-                                        reject(result);
-                                    }
-                                    else{
-                                        resolve('Số điện thoại hợp lệ');
-                                    }
-                                }
-                                if (j) {
-                                    reject("Số điện thoại di động không hợp lệ");
-                                }
-                            }
-                            if (v = y.split(","), k = 0, v != null && v != undefined && v.length > 0) {
-                                for (g = 0; g < v.length; g++) {
-                                    if (l.indexOf(v[g]) === 0) {
-                                        k = v[g];
-                                        break
-                                    }
-                                }
-                                if (g < v.length && !isNaN(l)) {
-                                    if ((k == "4" || k == "8") && l.length != 9) {
-                                        reject("Số điện thoại bàn phải là 10 số");
-                                    }
-                                    if (k != "4" && k != "8" && l.length - k.length != 7) {
-                                        reject("Số điện thoại bàn phải là " + (k.length + 8) + " số");
-                                    }
-                                } else {
-                                    reject("Số điện thoại không hợp lệ");
-                                }
-                            }
+                            //         if(result !== ''){
+                            //             reject(result);
+                            //         }
+                            //         else{
+                            //             resolve('Số điện thoại hợp lệ');
+                            //         }
+                            //     }
+                            //     if (j) {
+                            //         reject("Số điện thoại di động không hợp lệ");
+                            //     }
+                            // }
+                            // if (v = y.split(","), k = 0, v != null && v != undefined && v.length > 0) {
+                            //     for (g = 0; g < v.length; g++) {
+                            //         if (l.indexOf(v[g]) === 0) {
+                            //             k = v[g];
+                            //             break
+                            //         }
+                            //     }
+                            //     if (g < v.length && !isNaN(l)) {
+                            //         if ((k == "4" || k == "8") && l.length != 9) {
+                            //             reject("Số điện thoại bàn phải là 10 số");
+                            //         }
+                            //         if (k != "4" && k != "8" && l.length - k.length != 7) {
+                            //             reject("Số điện thoại bàn phải là " + (k.length + 8) + " số");
+                            //         }
+                            //     } else {
+                            //         reject("Số điện thoại không hợp lệ");
+                            //     }
+                            // }
+                            resolve('Số điện thoại hợp lệ');
                         }
                         resolve('Số điện thoại hợp lệ');
                     })
